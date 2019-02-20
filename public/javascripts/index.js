eaf536d0b9f349df97697444d76651efb0205c43
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-02-20 16:16:27 +0000

    Organize frontend js with es modules

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,21 @@
-var me = window.me || {}
+import { start as startMarquee, stop as stopMarquee } from './marquee.js'
 
-me.init = function () {
+function init () {
   var marqueeButtons = document.getElementsByClassName('info')
   for (var i = 0; i < marqueeButtons.length; i++) {
     marqueeButtons[i].addEventListener('click', function (event) {
-      var marquee = document.getElementById(`${event.target.id}-marquee`)
-      if (marquee.classList.contains('hidden')) {
-        marquee.classList.remove('hidden')
-        me.marquee.start(marquee)
+      var marqueeElement = document.getElementById(`${event.target.id}-marquee`)
+      if (marqueeElement.classList.contains('hidden')) {
+        marqueeElement.classList.remove('hidden')
+        startMarquee(marqueeElement)
       } else {
-        me.marquee.stop(marquee)
-        marquee.classList.add('hidden')
+        stopMarquee(marqueeElement)
+        marqueeElement.classList.add('hidden')
       }
     })
   }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  init()
+})
--- public/javascripts/marquee.js
+++ public/javascripts/marquee.js
@@ -1,10 +1,6 @@
-var me = window.me || {}
-
-me.marquee = {}
-
-me.marquee.start = function (element) {
-  element.addEventListener('mouseenter', this._mouseEnterHandler.bind(this))
-  element.addEventListener('mouseleave', this._mouseLeaveHandler.bind(this))
+export function start (element) {
+  element.addEventListener('mouseenter', mouseEnterHandler)
+  element.addEventListener('mouseleave', mouseLeaveHandler)
   var innerElement = document.createElement('span')
   innerElement.innerHTML = element.innerHTML
   innerElement.style.position = 'relative'
@@ -13,38 +9,38 @@ me.marquee.start = function (element) {
   element.appendChild(innerElement)
   element.style.overflow = 'hidden'
   element.style.whiteSpace = 'nowrap'
-  this._scroll(element)
+  scroll(element)
 }
 
-me.marquee.stop = function (element) {
-  element.removeEventListener('mouseenter', me.marquee._mouseEnterHandler)
-  element.removeEventListener('mouseleave', me.marquee._mouseLeaveHandler)
+export function stop (element) {
+  element.removeEventListener('mouseenter', mouseEnterHandler)
+  element.removeEventListener('mouseleave', mouseLeaveHandler)
   var innerElement = element.querySelector('span')
   element.innerHTML = innerElement.innerHTML
   element.removeAttribute('style')
-  window.cancelAnimationFrame(this._animationFrameRequestIds[element.id])
+  window.cancelAnimationFrame(animationFrameRequestIds[element.id])
 }
 
-me.marquee._scroll = function (element) {
+function scroll (element) {
   var innerElement = element.querySelector('span')
   var innerElementRightPosition = parseFloat(innerElement.style.right)
-  innerElementRightPosition += 5
+  innerElementRightPosition += 4
   var elementWidth = element.offsetWidth
   var innerElementWidth = innerElement.offsetWidth
   if (innerElementRightPosition > innerElementWidth) {
     innerElementRightPosition = -elementWidth
   }
   innerElement.style.right = `${innerElementRightPosition}px`
-  var id = window.requestAnimationFrame(this._scroll.bind(this, element))
-  this._animationFrameRequestIds[element.id] = id
+  var id = window.requestAnimationFrame(scroll.bind(this, element))
+  animationFrameRequestIds[element.id] = id
 }
 
-me.marquee._mouseEnterHandler = function (event) {
-  window.cancelAnimationFrame(this._animationFrameRequestIds[event.target.id])
+function mouseEnterHandler (event) {
+  window.cancelAnimationFrame(animationFrameRequestIds[event.target.id])
 }
 
-me.marquee._mouseLeaveHandler = function (event) {
-  window.requestAnimationFrame(this._scroll.bind(this, event.target))
+function mouseLeaveHandler (event) {
+  window.requestAnimationFrame(scroll.bind(this, event.target))
 }
 
-me.marquee._animationFrameRequestIds = {}
+var animationFrameRequestIds = {}
